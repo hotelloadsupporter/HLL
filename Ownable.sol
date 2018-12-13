2f80edd4984d8f7314a664372ebd44efeacaf3db
hotelloadsupporter <45853188+hotelloadsupporter@users.noreply.github.com>
2018-12-14 00:08:03 +0900

    Create Ownable.sol

---
+++ Ownable.sol
@@ -0,0 +1,14 @@
+
+contract Ownable {
+  address public owner;
+  address public admin;
+
+  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+  /**
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
+  constructor() public {
+    owner = msg.sender;
+  }
